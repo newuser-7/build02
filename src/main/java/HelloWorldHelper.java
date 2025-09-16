92e886f0f2c7d20fb79023927ae1ec5af13fb52a
xiang <cheng.xiang@regnology.net>
2025-09-16 11:18:49 +0000

    add unit test

---
+++ src/main/java/HelloWorldHelper.java
@@ -0,0 +1,8 @@
+public class HelloWorldHelper {
+public String useEnglish() {
+return "Hello World";
+}
+public String useSpanish() {
+return "Hello Mundo";
+}
+}
---
+++ src/test/java/HelloWorldHelperTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class HelloWorldHelperTest {
+@Test
+public void testEnglish() {
+HelloWorldHelper helper = new
+HelloWorldHelper();
+assertTrue(helper.useEnglish().contains("World"));
+}
+@Test
+public void testSpanish() {
+HelloWorldHelper helper = new
+HelloWorldHelper();
+assertTrue(helper.useSpanish().contains("Mundo"));
+}
+}
